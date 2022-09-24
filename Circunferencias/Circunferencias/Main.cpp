4045c177a442983998cd96c235ee0dc6e79ae8b3
BaudolinoII <espinodehorta99@gmail.com>
2022-09-24 12:14:31 -0500

    Actualizado

--- Circunferencias/Circunferencias/Main.cpp
+++ Circunferencias/Circunferencias/Main.cpp
@@ -24,9 +24,15 @@ const int complement(const float r) {
 		return (int)r;
 	return (int)(r + (r > 0 ? 1 : -1));
 }
-
 const int absolute(const int n) {
 	return (n > 0 ? n : n * -1);
+}
+const int aprox(const int destiny, const int position) {
+	if (destiny == position)
+		return position;
+	if (destiny > position)
+		return position + 1;
+	return position - 1;
 }
 
 typedef struct vector {
@@ -59,10 +65,17 @@ typedef struct node {
 typedef struct stack {
 	unsigned int size = 0;
 	Nodo* top = nullptr;
-	void push(const int x, const int y) {
+	int push(const int x, const int y) {
+		Nodo* it = top;
+		while (it != nullptr)
+			if (it->vec->x == x && it->vec->y == y) 
+				return 1;
+			else
+				it = it->next;
 		Nodo* nuevo = new Nodo(x, y, top);
 		top = nuevo;
 		size++;
+		return 0;
 	}
 	void printStack() {
 		Nodo* it = top;
@@ -72,7 +85,7 @@ typedef struct stack {
 		}
 		std::cout << "tamanio de pila: " << size << std::endl;
 	}
-	float* getArrayPoints(const float adjust) {
+	float* getArrayPoints(const float adjust = 1.0f) {
 		float* arrayPoints = new float[size * 3];
 		Nodo* it = top;
 		for (unsigned int i = 0; i < size; i++) {
@@ -83,48 +96,84 @@ typedef struct stack {
 		}
 		return arrayPoints;
 	}
+	unsigned int* getIndexArray(){
+		unsigned int* index = new unsigned int[2 * (size - 1)];
+		for (unsigned int i = 0; i < size - 1; i++) {
+			index[2 * i] = 0;
+			index[2 * i + 1] = i + 1;
+		}
+		return index;
+	}
 } Stack;
 
-float* setCircle(const unsigned int radio, Vector centro, const float adjust = 1.0f){
+Stack setCircle(const int radio, Vector centro){
 	Stack pila;
 	int before = centro.y, past = centro.y, current = 0, oposite = 0, otherside = radio + centro.x;
-	for (int i = (centro.x - radio); i < (radio + centro.x); i++) {
+	for (int i = (centro.x - radio); i <= (radio + centro.x); i++) {
 		current = complement(sqrt(radio * radio - (i - centro.x) * (i - centro.x))) + centro.y;
 		oposite = centro.y - complement(sqrt(radio * radio - (i - centro.x) * (i - centro.x)));
-		while(absolute(current - before) > 0){
+		while(absolute(current - before) != 0){
+			//std::cout << "[" << i - 1 << "," << before << "]" << std::endl;
 			pila.push(i - 1 , before);
+			//std::cout << "[" << otherside + 1 << "," << past << "]" << std::endl;
 			pila.push(otherside + 1, past);
-			before += (current > before ? 1 : -1);
-			past   += (oposite > past ? 1 : -1);
+			before = aprox(current, before);
+			past = aprox(oposite, past);
 		}
+		//std::cout << "[" << i << "," << current << "]" << std::endl;
 		pila.push(i, current);
+		//std::cout << "[" << otherside << "," << oposite << "]" << std::endl;
 		pila.push(otherside, oposite);
-		before += (current >= before ? 1 : -1);
-		past   += (oposite > past ? 1 : -1);
 		otherside--;
 	}
-	return pila.getArrayPoints(adjust);
+	pila.push(centro.x, centro.y);
+	//pila.printStack();
+	return pila;
 }
 
 int main() {
 	std::string args = "";
-	unsigned int radio;
+	unsigned int radio = 5;
 	/*std::cout << "Inserte Radio Circunferencia" << std::endl;
 	std::cin >> args;
 	radio = toInt(args);
 	std::cout << "Inserte Posicion en X,Y" << std::endl;
 	std::cin >> args;
 	Vector centro(args);*/
-	std::cout << complement(-9.5f) << std::endl;
-	std::cout << complement(-9.0f);
-	/*Stack pila;
-	pila.push(1, 2);
-	pila.push(2, 4);
-	pila.push(3, 8);
-	pila.push(4, 16);
-	pila.printStack();
-	float* arr = pila.getArrayPoints(1.5f);
-	for (int i = 0; i < 12; i++)
-		std::cout << " [" << arr[i] << "] ";*/
+	/*std::cout << complement(-9.5f) << std::endl;
+	std::cout << complement(-9.0f);*/
+	Vector centro(-2, 1);
+	Stack pila = setCircle(radio, centro);
+	float* arr = pila.getArrayPoints();
+	std::cout << "Arreglo de Vertices:\n(";
+	for (unsigned int i = 0; i < pila.size * 3; i++) {
+		std::cout << arr[i];
+		if ((i + 1) % 3 == 0)
+			std::cout << ")\t" << (i - 2) / 3 << "\n(";
+		else
+			std::cout << " , ";
+	}
+	unsigned int* idx = pila.getIndexArray();
+	std::cout << "\nArreglo de Indices:\n";
+	for (unsigned int i = 0; i < (pila.size - 1)* 2; i++) {
+		std::cout << idx[i];
+		std::cout << ((i + 1) % 2 == 0 ? "\n" : " , ");
+	}
+	//pila.push(1, 2);
+	//pila.push(1, 2);
+	//pila.push(1, 2);
+	//pila.push(2, 4);
+	//pila.push(3, 8);
+	//pila.push(4, 16);
+	//pila.printStack();
+	//float* arr = pila.getArrayPoints(1.5f);
+	//for (int i = 0; i < 4; i++)
+	//	std::cout << " [" << arr[i] << "] ";
+	
+	//for (int i = 0; i < 24; i++) {
+	//	std::cout << " [" << arr[i] << "] ";
+	//	if (i % 2 == 1)
+	//		std::cout << std::endl;
+	//}
 	return 0;
 }
